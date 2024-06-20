24dabb3296347b7d0ab0ce2ddea048d7da7f4b3c
victor schmoegel <victorschmoegel@gmail.com>
2024-06-20 16:02:58 -0300

    testando api

--- api/routes/route.go
+++ api/routes/route.go
@@ -1,1 +1,12 @@
 package routes
+
+import (
+	"log"
+	"net/http"
+	"processo-seletivo/api/controllers"
+)
+
+func HandleRequest() {
+	http.HandleFunc("/", controllers.Home)
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
