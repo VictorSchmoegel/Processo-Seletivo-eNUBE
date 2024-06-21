46f8e6fa64dcdf8dba602de4bb4076d4af9aab8b
victor schmoegel <victorschmoegel@gmail.com>
2024-06-21 17:44:36 -0300

    modificando rota para autenticação, colocando hash password jwt

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -1,7 +1,9 @@
 package routes
 
 import (
+	"net/http"
 	"processo-seletivo/api/controllers"
+	"processo-seletivo/api/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,5 +11,14 @@ import (
 func HandleRequests() *gin.Engine {
 	r := gin.Default()
 	r.POST("/createuser", controllers.CreateUser)
+	r.POST("/auth", controllers.AuthUser)
+	auth := r.Group("/")
+	auth.Use(middlewares.AuthMiddleware())
+	{
+		auth.GET("/protected-route", func(c *gin.Context) {
+			username := c.MustGet("username").(string)
+			c.JSON(http.StatusOK, gin.H{"message": "Hello " + username})
+		})
+	}
 	return r
 }
---
+++ api/utils/jwt.go
@@ -0,0 +1,68 @@
+package utils
+
+import (
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var JwtKey []byte
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		panic("No .env file found")
+	}
+	JwtKey = []byte(os.Getenv("SECRET_KEY"))
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func GenerateJWT(username string) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &Claims{
+		Username: username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(JwtKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func ValidateJWT(tokenString string) (*Claims, error) {
+	claims := &Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return JwtKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, err
+	}
+
+	return claims, nil
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
