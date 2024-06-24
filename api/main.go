170b573e3210ac9ddd9278441f426945f097edde
victor schmoegel <victorschmoegel@gmail.com>
2024-06-24 20:05:55 -0300

    configurando para deploy

--- api/main.go
+++ api/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"processo-seletivo/api/db"
 	"processo-seletivo/api/routes"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -10,5 +12,21 @@ func main() {
 	r := routes.HandleRequests()
 	if err := r.Run(":3000"); err != nil {
 		panic(err)
+	}
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
 	}
 }
