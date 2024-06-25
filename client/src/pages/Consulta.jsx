11005935b9fffc9ca4324f31b8571443ce3f7bd0
victor schmoegel <victorschmoegel@gmail.com>
2024-06-25 13:56:32 -0300

    organizando css da página de consulta

--- client/src/pages/Consulta.jsx
+++ client/src/pages/Consulta.jsx
@@ -121,164 +121,166 @@ export default function Consulta() {
   };
 
   return (
-    <main className="bg-slate-100 flex mx-auto">
-    <div className="flex flex-col gap-4 max-w-lg mx-auto p-20">
-      <h1 className="text-2xl font-bold mb-4 text-center">Consulta</h1>
-      <form className="flex flex-col gap-4">
+<main className="bg-slate-100 min-h-screen flex flex-col items-center justify-center py-10">
+      <div className="flex flex-col gap-4 w-full max-w-lg p-6 bg-white rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold mb-4 text-center">Consulta</h1>
+        <form className="flex flex-col gap-4">
+          {/* consulta de parceiros */}
+          <section className="border p-4 flex justify-between items-center rounded">
+            <h2 className="text-lg">Consulte Parceiros</h2>
+            <button
+              onClick={fetchPartners}
+              className="bg-blue-500 text-white px-4 py-2 rounded"
+              disabled={loading.partners}
+            >
+              {loading.partners ? 'Carregando...' : 'Consultar Parceiros'}
+            </button>
+          </section>
+          {errors.partners && <p className="text-red-500">{errors.partners}</p>}
 
-        {/* consulta de parceiros */}
-        <section className="border max-w-lg flex justify-between">
-          <h2 className="mt-auto mb-auto p-2 text-center">Consulte Parceiros</h2>
-          <button
-            onClick={fetchPartners}
-            className="bg-blue-500 text-white p-2 rounded"
-            disabled={loading.partners}
-          >
-            {loading.partners ? 'Carregando...' : 'Consultar Parceiros'}
-          </button>
+          {/* consulta de clientes */}
+          <section className="border p-4 flex justify-between items-center rounded">
+            <h2 className="text-lg">Consulte Clientes</h2>
+            <button
+              onClick={fetchCustomers}
+              className="bg-blue-500 text-white px-4 py-2 rounded"
+              disabled={loading.customers}
+            >
+              {loading.customers ? 'Carregando...' : 'Consultar Clientes'}
+            </button>
+          </section>
+          {errors.customers && <p className="text-red-500">{errors.customers}</p>}
+
+          {/* consulta de produtos */}
+          <section className="border p-4 flex justify-between items-center rounded">
+            <input
+              type="text"
+              placeholder="Nome ou ID do Produto"
+              value={productId}
+              onChange={(e) => setProductId(e.target.value)}
+              className="border p-2 rounded mr-2 flex-grow"
+            />
+            <button
+              onClick={fetchProduct}
+              className="bg-blue-500 text-white px-4 py-2 rounded"
+              disabled={loading.product}
+            >
+              {loading.product ? 'Carregando...' : 'Consultar Produto'}
+            </button>
+          </section>
+          {errors.product && <p className="text-red-500">{errors.product}</p>}
+
+          {/* consulta de clientes por país */}
+          <section className="border p-4 flex justify-between items-center rounded">
+            <input
+              type="text"
+              placeholder="País"
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+              className="border p-2 rounded mr-2 flex-grow"
+            />
+            <button
+              onClick={fetchCountryCustomers}
+              className="bg-blue-500 text-white px-4 py-2 rounded"
+              disabled={loading.countryCustomers}
+            >
+              {loading.countryCustomers ? 'Carregando...' : 'Consultar Clientes por País'}
+            </button>
+          </section>
+          {errors.countryCustomers && <p className="text-red-500">{errors.countryCustomers}</p>}
+        </form>
+      </div>
+
+      <div className="flex flex-col gap-4 w-full max-w-2xl mt-10 p-6 bg-white rounded-lg shadow-md">
+        <section>
+          {loading.partners ? '' : (
+            partners.length > 0 && (
+              <ul className="mt-4">
+                {partners.map((partner, index) => (
+                  <li key={index}>
+                    <div className="border p-4 rounded mb-4">
+                      <h3 className="text-lg font-semibold mb-2">Parceiro</h3>
+                      <p>Nome: {partner.partner_name}</p>
+                      <p>ID: {partner.partner_id}</p>
+                      <button
+                        className="bg-red-500 text-white px-4 py-2 rounded mt-2"
+                        onClick={() => setPartners([])}
+                      >
+                        Limpar
+                      </button>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            )
+          )}
         </section>
-        {errors.partners && <p className="text-red-500">{errors.partners}</p>}
 
-        {/* consulta de clientes */}
-        <section className="border max-w-lg flex justify-between">
-          <h2 className="mt-auto mb-auto p-2">Consulte Clientes</h2>
-          <button
-            onClick={fetchCustomers}
-            className="bg-blue-500 text-white p-2 rounded"
-            disabled={loading.customers}
-          >
-            {loading.customers ? 'Carregando...' : 'Consultar Clientes'}
-          </button>
-        </section>
-        {errors.customers && <p className="text-red-500">{errors.customers}</p>}
-
-        {/* consulta de produtos */}
-        <section className="border max-w-lg flex justify-between">
-          <input
-            type="text"
-            placeholder="Nome ou ID do Produto"
-            value={productId}
-            onChange={(e) => setProductId(e.target.value)}
-            className="border p-2 rounded mr-2"
-          />
-          <button
-            onClick={fetchProduct}
-            className="bg-blue-500 text-white p-2 rounded min-w-28"
-            disabled={loading.product}
-          >
-            {loading.product ? 'Carregando...' : 'Consultar Produto'}
-          </button>
-        </section>
-        {errors.product && <p className="text-red-500">{errors.product}</p>}
-
-        {/* consulta de clientes por país */}
-        <section className="border max-w-lg flex justify-between">
-          <input
-            type="text"
-            placeholder="País"
-            value={country}
-            onChange={(e) => setCountry(e.target.value)}
-            className="border p-2 rounded mr-2"
-          />
-          <button
-            onClick={fetchCountryCustomers}
-            className="bg-blue-500 text-white p-2 rounded"
-            disabled={loading.countryCustomers}
-          >
-            {loading.countryCustomers ? 'Carregando...' : 'Consultar Clientes por País'}
-          </button>
-        </section>
-        {errors.countryCustomers && <p className="text-red-500">{errors.countryCustomers}</p>}
-      </form>
-    </div>
-
-    <div className="flex flex-col gap-4 max-w-lg mx-auto p-20">
-      <section>
-        {loading.partners ? '' : (
-          partners.length > 0 && (
-            <ul className="mt-4">
-              {partners.map((partner, index) => (
-                <li key={index}>
-                  <div className="border font-semibold">
-                    <h3 className="text-center p-3 uppercase underline">Consulta de Parceiros</h3>
-                    <p>Nome do Parceiro: {partner.partner_name}</p>
-                    <p>Parceiro ID: {partner.partner_id}</p>
-                    <button
-                      className="bg-red-500 text-white p-2 rounded"
-                      onClick={() => setPartners([])}
-                    >
-                      Limpar
-                    </button>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          )
-        )}
-      </section>
-      <section>
-        {loading.customers ? '' : (
-          customers.length > 0 && (
-            <div>
-              <h3 className="text-center p-3 uppercase underline">Consulta de Clientes</h3>
-              <table className="table-auto w-full border-collapse border border-gray-200">
-                <thead>
-                  <tr>
-                    <th className="border p-2">Nome do Cliente</th>
-                    <th className="border p-2">Cliente ID</th>
-                    <th className="border p-2">Domínio</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {customers.map((customer, index) => (
-                    <tr key={index}>
-                      <td className="border p-2">{customer.customer_name}</td>
-                      <td className="border p-2">{customer.customer_id}</td>
-                      <td className="border p-2">{customer.customer_domain}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-              <button
-                className="bg-red-500 text-white p-2 rounded mt-4"
-                onClick={() => setCustomers([])}
-              >
-                Limpar
-              </button>
-            </div>
-          )
-        )}
-      </section>
-      <section>
-        {loading.product ? '' : (
-          product ? (
-            <div className="border flex flex-col gap-2">
-              <p className="font-semibold p-2">ID do Produto: {product.product_id}</p>
-              <p className="font-semibold p-2">Nome do produto: {product.product_name}</p>
-              <p className="font-semibold p-2">Quantidade: {product.quantity}</p>
-              <p className="font-semibold p-2">Preço Unitário: {product.unit_price}</p>
-              <button
-                className="bg-red-500 text-white p-2 rounded"
-                onClick={() => setProduct(null)}
-              >
-                Limpar
-              </button>
-            </div>
-          ) : (
-            errors.product && <p>{errors.product}</p>
-          )
-        )}
-      </section>
-      <section>
-          {loading.countryCustomers ? '' : (
-            countryCustomers.length > 0 ? (
+        <section>
+          {loading.customers ? '' : (
+            customers.length > 0 && (
               <div>
-                <h3 className="text-center p-3 uppercase underline">Consulta de Clientes por País</h3>
+                <h3 className="text-lg font-semibold mb-2">Clientes</h3>
                 <table className="table-auto w-full border-collapse border border-gray-200">
                   <thead>
                     <tr>
-                      <th className="border p-2">Nome do Cliente</th>
-                      <th className="border p-2">Cliente ID</th>
+                      <th className="border p-2">Nome</th>
+                      <th className="border p-2">Domínio</th>
+                      <th className="border p-2">ID</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {customers.map((customer, index) => (
+                      <tr key={index}>
+                        <td className="border p-2">{customer.customer_name}</td>
+                        <td className="border p-2">{customer.customer_domain}</td>
+                        <td className="border p-2">{customer.customer_id}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                <button
+                  className="bg-red-500 text-white px-4 py-2 rounded mt-4"
+                  onClick={() => setCustomers([])}
+                >
+                  Limpar
+                </button>
+              </div>
+            )
+          )}
+        </section>
+
+        <section>
+          {loading.product ? '' : (
+            product ? (
+              <div className="border p-4 rounded flex flex-col gap-2">
+                <p>ID: {product.product_id}</p>
+                <p>Nome: {product.product_name}</p>
+                <p>Quantidade: {product.quantity}</p>
+                <p>Preço: {product.unit_price}</p>
+                <button
+                  className="bg-red-500 text-white px-4 py-2 rounded mt-2"
+                  onClick={() => setProduct(null)}
+                >
+                  Limpar
+                </button>
+              </div>
+            ) : (
+              errors.product && <p className="text-red-500">{errors.product}</p>
+            )
+          )}
+        </section>
+
+        <section>
+          {loading.countryCustomers ? '' : (
+            countryCustomers.length > 0 ? (
+              <div>
+                <h3 className="text-lg font-semibold mb-2">Clientes por País</h3>
+                <table className="table-auto w-full border-collapse border border-gray-200">
+                  <thead>
+                    <tr>
+                      <th className="border p-2">Nome</th>
+                      <th className="border p-2">ID</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -291,7 +293,7 @@ export default function Consulta() {
                   </tbody>
                 </table>
                 <button
-                  className="bg-red-500 text-white p-2 rounded mt-4"
+                  className="bg-red-500 text-white px-4 py-2 rounded mt-4"
                   onClick={() => setCountryCustomers([])}
                 >
                   Limpar
@@ -302,7 +304,7 @@ export default function Consulta() {
             )
           )}
         </section>
-    </div>
-  </main>
+      </div>
+    </main>
   );
 }
